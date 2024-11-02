4bf443399ea012fd29c0d58a605b20c7ad12d7a2
aurora10 <aurora10@gmail.com>
2024-11-02 16:26:04 +0100

    h1123s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Marketorix Services",
+  title: "Marketorix Services AAA",
   description: "Transform Your Business with AI",
 };
 
