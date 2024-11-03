7165574bb7d240756b401c093f6587ee65bb29dc
aurora10 <aurora10@gmail.com>
2024-11-03 14:34:30 +0100

    rd3s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Red Bullet",
+  title: "XXXXXXXXXXXXXXXX",
   description: "Transform Your Business with AI anways from scratch",
 };
 
