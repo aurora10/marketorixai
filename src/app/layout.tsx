33443a722cf886e85637734c475660477700c52e
aurora10 <aurora10@gmail.com>
2024-11-03 14:05:46 +0100

    rd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Red Bullet Physics",
+  title: "Red Bullet",
   description: "Transform Your Business with AI",
 };
 
