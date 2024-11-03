b45aa62d134edd32ef48d582006f5c2e5ea3741d
aurora10 <aurora10@gmail.com>
2024-11-03 18:23:57 +0100

    /robert

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
     EMAIL_USER: process.env.EMAIL_USER,
     EMAIL_PASS: process.env.EMAIL_PASS,
   },
+  async redirects() {
+    return [
+      {
+        source: '/robert',
+        destination: 'https://robzi-portfollio.s3.eu-central-1.amazonaws.com/index-2.html',
+        permanent: true, // Use false for a temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
