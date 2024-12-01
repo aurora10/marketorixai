2fa73c319aa3352de685c957559324a3bf20378e
aurora10 <aurora10@gmail.com>
2024-12-01 16:41:48 +0100

    GTAG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleTagManager gtmId="GTM-KMS7X44" />
     </html>
   );
 }
